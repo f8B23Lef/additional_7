2a19bd7b79258fd6927660d4cfadba19633419a8
f8B23Lef <mal7rish@yandex.ru>
2018-09-21 16:50:49 +0300

    additional_7/implement function which solves sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  
+  var possibleValues = [1,2,3,4,5,6,7,8,9];
+    
+  for(var row = 0; row < 9; row++) {
+    for(var col = 0; col < 9; col++) {
+      if(matrix[row][col] === 0) {
+        for(var p = 0; p < possibleValues.length; p++) {
+          if(checkRow(possibleValues[p], row, matrix) && (checkColumn(possibleValues[p], col, matrix)) && (checkBlock(possibleValues[p], row, col, matrix))) {
+            matrix[row][col] = possibleValues[p];
+            if(solveSudoku(matrix)) {
+              return matrix;
+            }
+          }
+        }
+        matrix[row][col] = 0;
+        return false;
+      }
+    }
+  }
+  return matrix;
+}
+
+function checkRow(num, row, matrix) {
+  for(var col = 0; col < 9; col++) {
+    if(matrix[row][col] === num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkColumn(num, col, matrix) {
+  for(var row = 0; row < 9; row++) {
+    if(matrix[row][col] === num) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkBlock(num, row, col, matrix) {
+  var blockRow = Math.floor(row / 3) * 3;
+  var blockCol = Math.floor(col / 3) * 3;
+
+  for (var i = blockRow; i < (blockRow + 3); i++) {
+    for (var j = blockCol; j < (blockCol + 3); j++) {
+      if(matrix[i][j] === num) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
